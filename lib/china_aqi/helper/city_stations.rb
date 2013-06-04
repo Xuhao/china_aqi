106277e47997d1fbae48c4d916879d5f0c152182
Xuhao <xuhao@rubyfans.com>
2013-06-04 16:23:31 +0800

    MISC - DRY CityStations class

--- lib/china_aqi/helper/city_stations.rb
+++ lib/china_aqi/helper/city_stations.rb
@@ -1,17 +1,8 @@
 require 'china_aqi/utility'
 module ChinaAqi
   # All monitoring station names in one city
-  class CityStations
-    include ChinaAqi::Utility
-    
+  class CityStations < DynamicBase
     self.method = :station_names
-
-    def initialize(city)
-      raise TokenMissingError, 'Token is missing, run "rails g china_aqi:install" and set token in config/initializers/china_aqi.rb' unless ChinaAqi.token
-      @city = city
-      @token = ChinaAqi.token
-      @parmas = { city: city, token: ChinaAqi.token }
-    end
   end
 
   # create a helper in ChinaAqi module

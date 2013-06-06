64005933f99b788de9e49883adaa3e92c831aa65
Xuhao <xuhao@rubyfans.com>
2013-06-06 15:39:08 +0800

    MISC - Improve description for install generator

--- lib/generators/china_aqi/install/install_generator.rb
+++ lib/generators/china_aqi/install/install_generator.rb
@@ -1,12 +1,8 @@
 class ChinaAqi::InstallGenerator < Rails::Generators::Base
   def append_token_config
     application "config.china_aqi_token = 'you_token_here'"
-    puts ''
-    puts '-' * 66
-    puts "Find below line in config/application.rb:\n\n"
-    puts "    config.china_aqi_token = 'you_token_here'\n\n"
+    puts "Find below line in \e[33mconfig/application.rb\e[0m:"
+    puts "\e[32m  config.china_aqi_token = '\e[0m\e[31myou_token_here\e[0m\e[32m'\e[0m"
     puts "Put your token."
-    puts '-' * 66
-    puts ''
   end
 end
